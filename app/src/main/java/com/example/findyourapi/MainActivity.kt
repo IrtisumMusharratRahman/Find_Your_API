36ddcf99057377d8b236de6ac0e5a08faad80225
IrtisumMusharratRahman <rhythmfakir@gmail.com>
2022-12-19 23:41:48 +0600

    Demo UI with viewModel instance Error

--- app/src/main/java/com/example/findyourapi/MainActivity.kt
+++ app/src/main/java/com/example/findyourapi/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.findyourapi.ui.LandingPage.LandingPageScreen
 import com.example.findyourapi.ui.theme.FindYourAPITheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LandingPageScreen()
                 }
             }
         }
@@ -30,15 +28,5 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FindYourAPITheme {
-        Greeting("Android")
-    }
-}
+
--- app/src/main/java/com/example/findyourapi/ui/LandingPage/LandingPageScreen.kt
+++ app/src/main/java/com/example/findyourapi/ui/LandingPage/LandingPageScreen.kt
@@ -1,14 +1,53 @@
 package com.example.findyourapi.ui.LandingPage
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlin.text.Typography.dagger
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.findyourapi.model.Entrie
+
 
 @Composable
 fun LandingPageScreen(
-    viewModel: LandingPageViewModel
+    viewModel: LandingPageViewModel = viewModel()
 ){
-    viewModel.apis.collectAsState()
+    val apis = viewModel.apis.collectAsState()
+
+    LazyColumn() {
+        items(
+            items = apis.value.entries,
+            itemContent = {
+                ApiItem(entrie = it)
+            })
+    }
+}
+
+@Composable
+fun ApiItem(entrie: Entrie){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment= Alignment.CenterHorizontally,
+        modifier = Modifier.padding(5.dp).border(width = 1.dp, shape = RectangleShape, color = Color.Gray)
+    ) {
+        Text(text = entrie.API)
+        Text(text = entrie.Auth)
+        Text(text = entrie.Cors)
+        Text(text = entrie.HTTPS)
+        Text(text = entrie.Category)
+        Text(text = entrie.Description)
+        Text(text = entrie.Link)
+    }
 
 }
